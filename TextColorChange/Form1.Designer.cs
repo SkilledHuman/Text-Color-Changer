b4a6b104900b8cc037c51eefee0088d68a3c6dbe
User <User@DESKTOP-CGUDD6M>
2023-03-28 02:47:59 -0700

    Add project files.

---
+++ TextColorChange/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace TextColorChange
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnRed = new System.Windows.Forms.Button();
+            this.btnGreen = new System.Windows.Forms.Button();
+            this.btnBlue = new System.Windows.Forms.Button();
+            this.btnYellow = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.lblColor = new System.Windows.Forms.Label();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.btnColorDiag = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnCancel);
+            this.panel1.Controls.Add(this.btnSave);
+            this.panel1.Controls.Add(this.btnYellow);
+            this.panel1.Controls.Add(this.btnBlue);
+            this.panel1.Controls.Add(this.btnGreen);
+            this.panel1.Controls.Add(this.btnRed);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(128, 58);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(519, 307);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Georgia", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(139, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(234, 95);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Text";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnRed
+            // 
+            this.btnRed.Location = new System.Drawing.Point(45, 171);
+            this.btnRed.Name = "btnRed";
+            this.btnRed.Size = new System.Drawing.Size(75, 23);
+            this.btnRed.TabIndex = 1;
+            this.btnRed.Text = "Red";
+            this.btnRed.UseVisualStyleBackColor = true;
+            this.btnRed.Click += new System.EventHandler(this.btnRed_Click);
+            // 
+            // btnGreen
+            // 
+            this.btnGreen.Location = new System.Drawing.Point(166, 171);
+            this.btnGreen.Name = "btnGreen";
+            this.btnGreen.Size = new System.Drawing.Size(75, 23);
+            this.btnGreen.TabIndex = 2;
+            this.btnGreen.Text = "Green";
+            this.btnGreen.UseVisualStyleBackColor = true;
+            this.btnGreen.Click += new System.EventHandler(this.btnGreen_Click);
+            // 
+            // btnBlue
+            // 
+            this.btnBlue.Location = new System.Drawing.Point(287, 171);
+            this.btnBlue.Name = "btnBlue";
+            this.btnBlue.Size = new System.Drawing.Size(75, 23);
+            this.btnBlue.TabIndex = 3;
+            this.btnBlue.Text = "Blue";
+            this.btnBlue.UseVisualStyleBackColor = true;
+            this.btnBlue.Click += new System.EventHandler(this.btnBlue_Click);
+            // 
+            // btnYellow
+            // 
+            this.btnYellow.Location = new System.Drawing.Point(409, 171);
+            this.btnYellow.Name = "btnYellow";
+            this.btnYellow.Size = new System.Drawing.Size(75, 23);
+            this.btnYellow.TabIndex = 4;
+            this.btnYellow.Text = "Yellow";
+            this.btnYellow.UseVisualStyleBackColor = true;
+            this.btnYellow.Click += new System.EventHandler(this.btnYellow_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(181, 245);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 36);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(262, 245);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 36);
+            this.btnCancel.TabIndex = 6;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // lblColor
+            // 
+            this.lblColor.AutoSize = true;
+            this.lblColor.Location = new System.Drawing.Point(12, 9);
+            this.lblColor.Name = "lblColor";
+            this.lblColor.Size = new System.Drawing.Size(133, 16);
+            this.lblColor.TabIndex = 7;
+            this.lblColor.Text = "Selected color: None";
+            // 
+            // colorDialog1
+            // 
+            this.colorDialog1.FullOpen = true;
+            // 
+            // btnColorDiag
+            // 
+            this.btnColorDiag.Location = new System.Drawing.Point(15, 40);
+            this.btnColorDiag.Name = "btnColorDiag";
+            this.btnColorDiag.Size = new System.Drawing.Size(75, 49);
+            this.btnColorDiag.TabIndex = 7;
+            this.btnColorDiag.Text = "More Colors";
+            this.btnColorDiag.UseVisualStyleBackColor = true;
+            this.btnColorDiag.Click += new System.EventHandler(this.btnColorDiag_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnColorDiag);
+            this.Controls.Add(this.lblColor);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnYellow;
+        private System.Windows.Forms.Button btnBlue;
+        private System.Windows.Forms.Button btnGreen;
+        private System.Windows.Forms.Button btnRed;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblColor;
+        private System.Windows.Forms.Button btnColorDiag;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
+
---
+++ TextColorChange/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextColorChange
+{
+    public partial class Form1 : Form
+    {
+        string color = null;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRed_Click(object sender, EventArgs e)
+        {
+            lblColor.Text = "Selected color: Red";
+            color = "red";
+        }
+
+        private void btnGreen_Click(object sender, EventArgs e)
+        {
+            lblColor.Text = "Selected color: Green";
+            color = "green";
+        }
+
+        private void btnBlue_Click(object sender, EventArgs e)
+        {
+            lblColor.Text = "Selected color: Blue";
+            color = "blue";
+        }
+
+        private void btnYellow_Click(object sender, EventArgs e)
+        {
+            lblColor.Text = "Selected color: Yellow";
+            color = "yellow";
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+              switch (color)
+              {
+                 case "red": label1.ForeColor = Color.Red;
+                     break;
+                 case "green": label1.ForeColor = Color.Green;
+                     break;
+                 case "blue": label1.ForeColor = Color.Blue;
+                     break;
+                 case "yellow": label1.ForeColor = Color.Yellow;
+                     break;
+              }
+
+            if(color == null)
+            {
+                label1.ForeColor = colorDialog1.Color;
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            lblColor.Text = "Selected color: None";
+            color = null;
+        }
+
+        private void btnColorDiag_Click(object sender, EventArgs e)
+        {
+            color = null;
+            colorDialog1.ShowDialog();
+            string dialogColor = colorDialog1.Color.ToString();
+            lblColor.Text = "Selected color: " + dialogColor;
+        }
+    }
+}
